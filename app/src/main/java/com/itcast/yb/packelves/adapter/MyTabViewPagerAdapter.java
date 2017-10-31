454edc7e2de4ce5952100a53259404742c6529db
617490820@qq.com <5070363>
2017-10-31 11:24:15 +0800

    23

--- app/src/main/java/com/itcast/yb/packelves/adapter/MyTabViewPagerAdapter.java
+++ app/src/main/java/com/itcast/yb/packelves/adapter/MyTabViewPagerAdapter.java
@@ -3,6 +3,8 @@ package com.itcast.yb.packelves.adapter;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
+import android.view.ViewGroup;
 
 import com.itcast.yb.packelves.fragment.BaseFragment;
 import com.itcast.yb.packelves.fragment.FragmentFactory;
@@ -15,6 +17,8 @@ public class MyTabViewPagerAdapter extends FragmentPagerAdapter{
     private String[] mTitle = null;
     private int mflag=0;
     BaseFragment fragment=null;
+    String mTags="";
+
     public MyTabViewPagerAdapter(FragmentManager fm,String[] Title,int flag) {
         super(fm);
         mTitle=Title;
@@ -35,7 +39,7 @@ public class MyTabViewPagerAdapter extends FragmentPagerAdapter{
     @Override
     public Fragment getItem(int position) {
         if(mflag==1){
-             fragment = FragmentFactory.createFeatureFg(position);
+                fragment = FragmentFactory.createFeatureFg(position);
         }else if(mflag==0){
              fragment = FragmentFactory.createOpengameFg(position);
         }
@@ -46,4 +50,11 @@ public class MyTabViewPagerAdapter extends FragmentPagerAdapter{
     public int getCount() {
         return mTitle.length;
     }
+//
+//    @Override
+//    public Object instantiateItem(ViewGroup container, int position) {
+//        Fragment fragment = (Fragment) super.instantiateItem(container, position);
+//            mTags  = fragment.getTag();
+//        return fragment;
+//    }
 }
