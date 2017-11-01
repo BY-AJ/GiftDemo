e44ee23012d702a00d21c0bfcff49762586b0bc1
617490820@qq.com <5070363>
2017-11-01 16:54:30 +0800

    1111111

---
+++ app/src/main/java/com/itcast/yb/packelves/utils/CustomLinearLayoutManager.java
@@ -0,0 +1,26 @@
+package com.itcast.yb.packelves.utils;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+
+/**
+ * Created by lenovo on 2017/11/1.
+ */
+public class CustomLinearLayoutManager extends LinearLayoutManager {
+    private boolean isScrollEnabled = true;
+
+    public CustomLinearLayoutManager(Context context) {
+        super(context);
+    }
+
+    public void setScrollEnabled(boolean flag) {
+        this.isScrollEnabled = flag;
+    }
+
+    @Override
+    public boolean canScrollVertically() {
+        //Similarly you can customize "canScrollHorizontally()" for managing horizontal scroll
+        return isScrollEnabled && super.canScrollVertically();
+    }
+}
+
