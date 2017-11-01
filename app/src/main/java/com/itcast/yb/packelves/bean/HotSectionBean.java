24aa254c9479a611b3b4af17bc5bfdba29a6b7b5
617490820@qq.com <5070363>
2017-11-01 14:15:17 +0800

    hot1

---
+++ app/src/main/java/com/itcast/yb/packelves/bean/HotSectionBean.java
@@ -0,0 +1,13 @@
+package com.itcast.yb.packelves.bean;
+
+/**
+ * Created by lenovo on 2017/11/1.
+ */
+public class HotSectionBean {
+   public String name;//扑克捕鱼
+    public  String typename;//类型
+    public   String size;//大小
+    public  String appId;//
+    public String clicks;//点击人数
+    public  String logo;//图标
+}
