a37ba342c9f39989d6ef991e0ea653271533b5a5
617490820@qq.com <5070363>
2017-11-06 15:09:15 +0800

    loglient

--- app/src/main/java/com/itcast/yb/packelves/module/LoginClient.java
+++ app/src/main/java/com/itcast/yb/packelves/module/LoginClient.java
@@ -2,11 +2,12 @@ package com.itcast.yb.packelves.module;
 
 import com.itcast.yb.packelves.bean.LoginBean;
 
+import java.util.Map;
+
 import retrofit2.Call;
-import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
 
 /**
  * Created by yb on 2017/11/6.
@@ -16,5 +17,5 @@ public interface LoginClient {
     //http://www.1688wan.com//majax.action?method=mobileUserLogin
     @POST("method=mobileUserLogin")
     @FormUrlEncoded
-    Call<LoginBean> login(@Query("name") String name, @Query("pwd") String psw);
+    Call<LoginBean> login(@FieldMap Map<String,String> map);
 }
