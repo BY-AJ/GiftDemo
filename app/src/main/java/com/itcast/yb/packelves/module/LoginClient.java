ccdc13760093a053ccd6b1d555e2ad793f9e8e33
617490820@qq.com <5070363>
2017-11-06 14:46:25 +0800

    注册1

--- app/src/main/java/com/itcast/yb/packelves/module/LoginClient.java
+++ app/src/main/java/com/itcast/yb/packelves/module/LoginClient.java
@@ -6,6 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 /**
  * Created by yb on 2017/11/6.
@@ -15,5 +16,5 @@ public interface LoginClient {
     //http://www.1688wan.com//majax.action?method=mobileUserLogin
     @POST("method=mobileUserLogin")
     @FormUrlEncoded
-    Call<LoginBean> login(@Field("name") String name,@Field("pwd") String psw);
+    Call<LoginBean> login(@Query("name") String name, @Query("pwd") String psw);
 }
