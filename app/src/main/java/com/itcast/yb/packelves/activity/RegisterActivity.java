aa4280fd002888773aa258094d79d1c1ed4a42be
617490820@qq.com <5070363>
2017-11-06 18:18:47 +0800

    headtitle2

--- app/src/main/java/com/itcast/yb/packelves/activity/RegisterActivity.java
+++ app/src/main/java/com/itcast/yb/packelves/activity/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.itcast.yb.packelves.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,6 +11,7 @@ import com.itcast.yb.packelves.BaseActivity;
 import com.itcast.yb.packelves.R;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class RegisterActivity extends BaseActivity {
@@ -19,6 +21,9 @@ public class RegisterActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        ButterKnife.bind(this);
         initBasic();
     }
 
